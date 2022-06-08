f246bfe3f2d652194d1e496baec45cb61c1d721d
anabartos <ana.l.bartos@gmail.com>
2022-06-08 16:39:11 +0000

    Random card con js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,40 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const numbers = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "k",
+    "Q",
+    "A"
+  ];
+  const palo = ["♦", "♥", "♠", "♣"];
+  let header = document.querySelector("#card_Header");
+  let footer = document.querySelector("#card_Footer");
+  let body = document.querySelector(".body");
+
+  const random = value => {
+    let index = Math.floor(Math.random() * value.length);
+    return value[index];
+  };
+
+  const symbol = random(palo);
+
+  if (symbol == "♦" || symbol == "♥") {
+    header.style.color = "red";
+    footer.style.color = "red";
+  }
+
+  body.textContent = random(numbers);
+  header.textContent = symbol;
+  footer.textContent = symbol;
 };
